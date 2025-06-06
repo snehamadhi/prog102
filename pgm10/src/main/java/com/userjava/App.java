950c596f3cb992c72242be326e7d85ab58da429d
myusername <snm22ise@cmrit.ac.in>
2025-06-06 17:02:01 +0530

    sneha

---
+++ pgm10/src/main/java/com/userjava/App.java
@@ -0,0 +1,13 @@
+package com.userjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
